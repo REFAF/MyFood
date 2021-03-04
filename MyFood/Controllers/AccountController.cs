706ec8e6b258dd89e67b7561e0dbb400a56a4911
MAI-IT <52855701+MAI-IT@users.noreply.github.com>
2021-03-04 19:43:04 +0300

    SmallChanges

--- MyFood/Controllers/AccountController.cs
+++ MyFood/Controllers/AccountController.cs
@@ -211,7 +211,7 @@ namespace MyFood.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser {userType_id = 4, UserName = model.Email, Email = model.Email
+                var user = new ApplicationUser {userType_id = 1, UserName = model.Email, Email = model.Email
                 , PhoneNumber = model.PhoneNumber, name = model.name};
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
@@ -299,6 +299,42 @@ namespace MyFood.Controllers
             return View(model);
         }
 
+        [AllowAnonymous]
+        public async Task<ActionResult> Inv(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var user = new ApplicationUser
+                {
+                    userType_id = 2,
+                    UserName = model.PhoneNumber,
+                    PhoneNumber = model.PhoneNumber,
+                    PasswordHash = model.PhoneNumber
+                    
+                };
+                var result = await UserManager.CreateAsync(user, model.Password);
+
+
+                if (result.Succeeded)
+                {
+
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    return RedirectToAction("AddPhoneNumber", "Manage");
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
         //
         //// GET: /Account/Register
         //[AllowAnonymous]
@@ -374,12 +410,12 @@ namespace MyFood.Controllers
                     return View("ForgotPasswordConfirmation");
                 }
 
-                // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                // Send an email with this link
-                // string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
-                // var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
-                // await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                // return RedirectToAction("ForgotPasswordConfirmation", "Account");
+                //For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                //Send an email with this link
+                 string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
+                var callbackUrl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                return RedirectToAction("ForgotPasswordConfirmation", "Account");
             }
 
             // If we got this far, something failed, redisplay form
