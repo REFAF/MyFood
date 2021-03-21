26ce95e3ca151905890ee729b4bf09271952bfc9
MAI-IT <52855701+MAI-IT@users.noreply.github.com>
2021-03-21 09:10:21 +0300

    ApplyingDataTable

--- MyFood/App_Start/BundleConfig.cs
+++ MyFood/App_Start/BundleConfig.cs
@@ -9,7 +9,10 @@ namespace MyFood
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/datatables/jquery.datatables.js",
+                        "~/Scripts/datatables/datatables.bootstrap.js"
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -31,6 +34,7 @@ namespace MyFood
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/bootstrap.rtl.css",
                       "~/Content/bootstrap.rtl.full.css",
                       "~/Content/bootstrap.rtl.full.min.css",
