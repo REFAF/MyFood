ad140422f5ea6b3ffa045c77b54fe3087fd76d28
refaf222@gmail.com <refaf222@gmail.com>
2021-04-05 09:06:42 +0300

    UI-pt2

--- MyFood/App_Start/BundleConfig.cs
+++ MyFood/App_Start/BundleConfig.cs
@@ -32,14 +32,43 @@ namespace MyFood
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            //bundles.Add(new ScriptBundle("~/bundles/js").Include(
+            //          "~/Scripts/js/soft-design-system.js",
+            //          "~/Scripts/js/soft-design-system.min.js",
+            //          "~/Scripts/js/core/popper.min.js",
+            //          "~/Scripts/js/plugins/choices.min.js",
+            //          "~/Scripts/js/plugins/countup.min.js",
+            //          "~/Scripts/js/plugins/flatpickr.min.js",
+            //          "~/Scripts/js/plugins/moment.min.js",
+            //          "~/Scripts/js/plugins/parallax.min.js",
+            //          "~/Scripts/js/plugins/perfect-scrollbar.min.js",
+            //          "~/Scripts/js/plugins/prism.min.js",
+            //          "~/Scripts/js/plugins/rellax.min.js",
+            //          "~/Scripts/js/plugins/tilt.min.js",
+            //          "~/Scripts/js/plugins/typedjs.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/Customize.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
-                      "~/Content/bootstrap.rtl.css",
-                      "~/Content/bootstrap.rtl.full.css",
-                      "~/Content/bootstrap.rtl.full.min.css",
-                      "~/Content/bootstrap.rtl.min",
-                      "~/Content/site.css"));
+
+                      "~/Content/bootstrap-rtl/bootstrap.rtl.css",
+                      "~/Content/bootstrap-rtl/bootstrap.rtl.full.css",
+                      "~/Content/bootstrap-rtl/bootstrap.rtl.full.min.css",
+                      "~/Content/bootstrap-rtl/bootstrap.rtl.min"
+                      
+                      //"~/Content/Tailwind/compiled-tailwind.css",
+                      //"~/Content/Tailwind/compiled-tailwind.min.css",
+                      //"~/Content/Tailwind/tailwind.css",
+                      //"~/Content/Tailwind/tailwind.min.css",
+
+                      //"~/Content/soft-design/nucleo-icons.css",
+                      //"~/Content/soft-design/nucleo-svg.css",
+                      //"~/Content/soft-design/soft-design-system.css",
+                      //"~/Content/soft-design/soft-design-system.min.css"
+
+                      ));
 
         }
     }
